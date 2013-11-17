ee0e58824a94d43fe21f4c5f7bc9a5af77b8a8e5
ally palanzi <ampalanzi@gmail.com>
2013-11-17 18:52:34 -0500

    idk what i'm doing

--- config.rb
+++ config.rb
@@ -36,7 +36,7 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# activate :livereload
+ activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -49,12 +49,12 @@ set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
 
-set :images_dir, 'images'
+set :images_dir, 'img'
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
